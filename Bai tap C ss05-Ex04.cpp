9fdeb90cd1f3136105f142fb7c32680b707ab75e
Zarck299 <honganhzarck@gmail.com>
2024-11-26 10:01:11 +0700

    Update Bai tap C ss05-Ex04.cpp

--- Bai tap C ss05-Ex04.cpp
+++ Bai tap C ss05-Ex04.cpp
@@ -4,10 +4,10 @@
    	do { 
    	printf("Hay nhap mot so nguyen duong(Tu 1 den 10): ");
    	scanf("%d", &n);
-   	if ( n <= 1 || n >= 10) {
+   	if ( n < 1 || n > 10) {
    		printf("So nhap vao khong hop le. Hay nhap lai\n");
 	   }
-	 }while (n <= 1 || n>= 10); {
+	 }while (n < 1 || n > 10); {
 	 printf("Bang cuu chuong cua %d:\n", n);
     for (int i = 1; i <= 10; i++) {
         printf("%d x %d = %d\n", n, i, n * i);
